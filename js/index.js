a574f711de3a36b47518e1909cac3517d034dacd
Fahrenheit-29 <RenFah29@gmail.com>
2023-06-17 12:02:20 +0800

    Deploy Note Taking App

---
+++ js/index.js
@@ -0,0 +1,168 @@
+// Select the modal
+
+let modal = document.querySelector(".modalView");
+let closeBtn = document.querySelector(".close");
+
+
+// Select inputField value
+
+
+let noteTitle = document.getElementById('noteTitle');
+let notesInput = document.getElementById('note');
+
+
+
+// Select the note-taking content to appendchild
+
+let noteLayout = document.querySelector(".noteLayout");
+
+
+// Select addBtn
+
+let addNotesBtn = document.getElementById('addBtn');
+let col;
+
+
+// use LocalStorage
+
+const saveData = () => {
+
+    localStorage.setItem("data", noteLayout.innerHTML);
+
+
+}
+const getData = () => {
+
+
+    noteLayout.innerHTML = localStorage.getItem("data");
+
+}
+
+// Use click event listener
+
+addNotesBtn.addEventListener('click', (e) => {
+
+
+    if (checkInputField(noteTitle) || checkInputField(notesInput)) {
+
+        alert("Input all fields");
+
+    } else {
+
+
+
+
+        let title = noteTitle.value;
+        let noteData = notesInput.value;
+
+
+
+        // create a col div
+        col = document.createElement('div');
+        col.classList.add('col-notes', 'my-3');
+
+        noteLayout.appendChild(col);
+        col.innerHTML = `
+
+    <h4 class="titleNote">${title}</h4>
+
+    <p class="notes">${trimContent(noteData)}</p>
+
+
+    <div class="Btn-data my-3 text-center">
+        <button class="btn btn-sm btn-dark viewDetail">View Detail</button>
+        <button class="btn btn-sm btn-danger deleteBtn">Delete</button>
+    </div>
+
+        `;
+
+        // target View Detail button
+        col.addEventListener('click', (e) => {
+
+            if (e.target.classList.contains("viewDetail")) {
+
+                showModal(noteData);
+            }
+
+        });
+        saveData();
+
+
+        // clear input & textArea value
+        noteTitle.value = "";
+        notesInput.value = "";
+    }
+
+
+});
+
+//localstorage to get Data
+
+getData();
+
+// delete Note Data
+noteLayout.addEventListener("click", function(e) {
+
+
+    if (e.target.innerText === "Delete") {
+
+        e.target.parentElement.parentElement.remove();
+        saveData();
+
+
+    }
+});
+
+
+
+// closeBtn modal function
+closeBtn.addEventListener("click", () => {
+
+    modal.style.display = "none";
+
+
+});
+
+
+document.addEventListener("click", (e) => {
+
+    if (!e.target.classList.contains("viewDetail")) {
+
+        modal.style.display = "none";
+    }
+
+});
+
+
+
+// all functions
+
+
+const checkInputField = (noteData) => {
+
+
+    return noteData.value === '' || noteData.value.trim().length === 0;
+
+
+}
+
+
+// trim Data function
+const trimContent = (data) => {
+
+
+    let trimData = data.slice(0, 20);
+    return trimData += "...";
+
+}
+
+// showModal function
+const showModal = (noteValue) => {
+    let noteContent = document.querySelector(".noteContent");
+    modal.style.display = "block";
+
+    // Note Content Inside Modal
+    noteContent.innerText = `${noteValue}`;
+
+
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Note Taking App
+
+## Description
+
+    An app create in Vanilla Javascript that use to write your thoughts and keep track information for yourself
+
+![Note App ](image/Note HomePage.png)
+
+
+### Technology Used
+
+* HTML & CSS
+* BOOTSTRAP
+* VANILLA JAVASCRIPT
